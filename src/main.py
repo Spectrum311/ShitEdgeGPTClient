63fff815ae7c875b4b8e9627673f461cd687cf92
Spectrum311 <129543357+Spectrum311@users.noreply.github.com>
2023-04-07 21:08:31 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
+import EdgeGPT
+import asyncio
+
 bot = Chatbot(cookiePath='ShitEdgeGPTClient/cookies.json')
 
 with open('./cookie.json', 'r') as f:
     cookies = json.load(f)
 bot = Chatbot(cookies=cookies)
+
+
+async def main():
+    bot = Chatbot()
+    print(await bot.ask(prompt="Hello world", conversation_style=ConversationStyle.creative, wss_link="wss://sydney.bing.com/sydney/ChatHub"))
+    await bot.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
